7bf93e33d1a705a04162932f56faddd7baf76d53
陈芳芳 <2186009311@qq.com>
2021-01-14 20:41:25 +0800

    搜索框

---
+++ pages/index/index.vue
@@ -0,0 +1,39 @@
+<template>
+	<view >
+		<search preholder="搜索附近位置" @toSearch="toSearchFun"></search>
+		<search preholder="搜索附近位置2" :isToAll="isToAll" :themeColor="themeColor" @toSearch="toSearchFun" @toAll="toAllFun"></search>
+	</view>
+</template>
+
+<script>
+	import search from '@/components/ay-search/search.vue';
+	export default {
+		components: {
+			search,
+
+		},
+		data() {
+			return {
+				themeColor:'#33CCCC',
+				isToAll : true ,
+			}
+		},
+		onLoad() {
+			let that = this;
+
+		},
+
+		methods: {
+			toSearchFun(e) {
+				
+			},
+			toAllFun(){
+				
+			},
+		}
+	}
+</script>
+
+<style lang="scss">
+	
+</style>
